ef37a6ba0f3300481719f19b9b48c0fa1c632181
Dev.NightFury <blacktiny126@hotmail.com>
2024-06-20 12:06:00 -0400

    feat: add the preview for the draft project

--- pages/preview.js
+++ pages/preview.js
@@ -1,5 +1,90 @@
-import { WordPressTemplate } from '@faustwp/core';
+import React from 'react';
+import { gql } from '@apollo/client';
+import { getApolloAuthClient } from '@faustwp/core';
+import { useRouter } from 'next/router';
+import ProjectContent from '../components/Project/ProjectContent';
 
 export default function Preview(props) {
-  return <WordPressTemplate {...props} />;
+  const { query } = useRouter();
+  const client = getApolloAuthClient();
+  const id = query.p;
+  const isPreview = true;
+  
+  const [previewProject, setPreviewProject] = React.useState();
+  
+  client.query({
+    query: gqlquery,
+    variables: {
+      id,
+      idType: isPreview ? 'DATABASE_ID' : 'URI',
+      asPreview: isPreview,
+    },
+  }).then(({ data }) => {
+    setPreviewProject(data.contentNode);
+  });
+
+  if (!previewProject) return <></>;
+
+  return (
+    <ProjectContent project={previewProject} nextProject={null} />
+  );
 }
+
+const gqlquery = gql`
+  query GetContentNode(
+    $id: ID!
+    $idType: ContentNodeIdTypeEnum!
+    $asPreview: Boolean!
+  ) {
+    contentNode(id: $id, idType: $idType, asPreview: $asPreview) {
+      ... on Project {
+        title
+        projectsSingle {
+          projectDetails {
+            attributes {
+              label
+              attributeListings {
+                link
+                title
+              }
+            }
+            label
+          }
+          projectImages {
+            description
+            image {
+              node {
+                altText
+                mediaItemUrl
+                mediaDetails {
+                  width
+                  height
+                }
+              }
+            }
+            video {
+              node {
+                altText
+                mediaItemUrl
+                mediaDetails {
+                  height
+                  width
+                }
+              }
+            }
+          }
+        }
+        featuredImage {
+          node {
+            altText
+            mediaDetails {
+              height
+              width
+            }
+            mediaItemUrl
+          }
+        }
+      }
+    }
+  }
+`;
