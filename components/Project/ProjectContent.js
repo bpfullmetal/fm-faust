67db37938a80801f645e8c337b8118a3f01c6460
Dev.NightFury <blacktiny126@hotmail.com>
2024-07-02 21:37:30 -0400

    fix: remove the sizes property for hero images

--- components/Project/ProjectContent.js
+++ components/Project/ProjectContent.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Image from 'next/image';
 import Helper from '../../helper';
-import { PageLayout } from '../../components';
 import ProjectCarouselModal from './CarouselModal';
 import NextProject from './NextProject';
 
@@ -155,218 +154,213 @@ const ProjectContent = ({ project, scrollContainerRef, isPreview = false }) => {
   };
 
   return (
-    // <PageLayout
-    //   options={{ currentURI: '/work/' }}
-    //   pageData={project}
-    // >
-      <>
-        {featuredImage && (
-          <section className="h-home_banner">
-            <div className="relative w-full h-full flex items-center justify-center">
-              <Image
-                className="featured-image-wrapper w-full h-full object-cover rounded-none"
-                src={featuredImage.node.mediaItemUrl}
-                layout="fill"
-                sizes="(min-width: 1024px) 50vw, 70vw"
-                loading="eager"
-                priority={true}
-                alt={featuredImage.node.altText || title}
-              />
-              <h1 className="absolute max-w-[480px] text-4xl font-medium !leading-none text-center md:max-w-[580px] md:text-5xl lg:max-w-[680px] lg:text-[58px]">
-                {title}
-              </h1>
-            </div>
-          </section>
-        )}
-
-        <section className="work-project flex flex-col bg-[#300808]">
-          <div className="w-full max-w-main mx-auto px-5 sm:px-12 pb-16 sm:pb-40">
-            {!featuredImage && (
-              <h1 className="mx-auto mt-12 mb-12 max-w-[480px] text-3xl font-medium !leading-none text-center md:max-w-[580px] md:text-4xl lg:max-w-[680px] lg:text-[38px] text-center">
-                {title}
-              </h1>
-            )}
-            {(project.content ||
-              projectsSingle?.projectDetails?.attributes?.length) && (
-              <div className="flex flex-col mt-8">
-                <div
-                  className="flex items-center cursor-pointer"
-                  onClick={() => setRevealProjectInfo((old) => !old)}
-                >
-                  <div
-                    className={`relative flex items-center justify-center w-6 h-6 mr-2 sm:mr-6 transition-all ease-out duration-300 ${
-                      revealProjectInfo ? 'rotate-[135deg]' : 'rotate-0'
-                    }`}
-                  >
-                    <div className="absolute w-4 h-0.5 bg-taupe"></div>
-                    <div className="absolute w-0.5 h-4 bg-taupe"></div>
-                  </div>
-                  <p className="text-taupe text-xl leading-[44px] sm:text-[26px]">
-                    {projectsSingle.projectDetails.label ||
-                      'Project Information'}
-                  </p>
-                </div>
-                <div
-                  className={`text-taupe w-full md:w-1/2 h-0 ${
-                    revealProjectInfo ? 'h-full pt-2' : 'pt-0'
-                  } flex flex-col pl-8 sm:pl-12 pr-4 md:pr-0 overflow-hidden transition-all`}
-                >
-                  {/* {project.content && (
-                    <div
-                      className="space-y-12"
-                      dangerouslySetInnerHTML={{
-                        __html: project.projectsSingle.projectDetails.content,
-                      }}
-                    />
-                  )} */}
-                  {projectsSingle.projectDetails.attributes && (
-                    <div className="project-details">
-                      {projectsSingle.projectDetails.attributes.map(
-                        (attribute, i) => (
-                          <div
-                            key={`project-details-${i}`}
-                            className="flex flex-col pt-9 space-y-2"
-                          >
-                            {attribute.label && (
-                              <p className="text-taupe text-xs">
-                                {attribute.label}
-                              </p>
-                            )}
-                            {attribute.attributeListings && (
-                              <div className="text-taupe text-lg leading-[34px] sm:text-[20px]">
-                                {attribute.attributeListings.map(
-                                  (attItem, a) => {
-                                    return (
-                                      <div key={`project-attribute-${a}`}>
-                                        {attItem.link ? (
-                                          <a
-                                            target="_blank"
-                                            href={attItem.link}
-                                            rel="noreferrer"
-                                          >
-                                            <span>{attItem.title}</span>
-                                          </a>
-                                        ) : (
-                                          <>{attItem.title}</>
-                                        )}
-                                        {a !==
-                                          attribute.attributeListings.length -
-                                            1 && <span>, </span>}
-                                      </div>
-                                    );
-                                  }
-                                )}
-                              </div>
-                            )}
-                          </div>
-                        )
-                      )}
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-            {projectImages.length ? (
-              <div
-                id="project-images-container"
-                ref={scrollContainerRef}
-                className="flex flex-col mt-8 md:mt-20"
-              >
-                {projectImages.map((block, i) => {
-                  const blockPos = imageBlockPositions[i];
-                  const blockSize = imageBlockSizes[i];
-                  const imageBlock = block.image ? block.image : block.video;
-                  if (!imageBlock) return <></>;
-                  const orientation =
-                    imageBlock.node.mediaDetails.width <=
-                    imageBlock.node.mediaDetails.height
-                      ? 'portrait'
-                      : 'landscape';
-                      
-                  return (
-                    <div
-                      ref={projectRefs[i]}
-                      className={`project-block${
-                        projectImages.length !== i + 1 ? ' mb-20 md:mb-40' : ''
-                      } flex flex-col-reverse items-start ${
-                        pos2Class[blockPos]
-                      } sm:items-center`}
-                      key={`project-image-${i}`}
-                    >
-                      <div className="description-reveal flex-1 text-taupe text-sm_extra leading-[24px]">
-                        {block.description && block.description}
-                      </div>
-                      {(block.image || block.video) && (
-                        <div
-                          className={`image-reveal image-to-lightbox ${size2Class[blockSize]} relative ${orientation === 'landscape'
-                            ? 'aspect-[4/3]'
-                            : 'aspect-[3/4]'}`}
-                          onClick={() =>
-                            !isMobile ? setClickedImageOrder(i) : null
-                          }
-                        >
-                          {block.video ? (
-                            <div>
-                              <video
-                                autoPlay
-                                muted
-                                loop
-                                onLoadedData={() =>
-                                  setImageBlockDetails(
-                                    imageBlockDetails.map((item, index) =>
-                                      index === i
-                                        ? { ...item, isLoaded: true }
-                                        : item
-                                    )
-                                  )
-                                }
-                                className="w-full h-full"
-                              >
-                                <source
-                                  src={block.video.node.mediaItemUrl}
-                                  type="video/mp4"
-                                />
-                              </video>
-                            </div>
-                          ) : (
-                            <Image
-                              className={`featured-image-wrapper w-full h-auto rounded featured-image-${block.image.node.id}`}
-                              src={block.image.node.mediaItemUrl}
-                              layout="fill"
-                              objectFit="cover"
-                              objectPosition="center"
-                              sizes="(min-width: 1024px) 40vw, 70vw"
-                              loading="eager"
-                              onLoad={event => handleDisplayImage(event, projectRefs[i])}	
-                              quality={82}
-                              alt={
-                                block.image.node.altText ||
-                                block.description ||
-                                `${title} image ${i + 1}`
-                              }
-                            />
-                          )}
-                        </div>
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-            ) : <></>}
+    <>
+      {featuredImage && (
+        <section className="h-home_banner">
+          <div className="relative w-full h-full flex items-center justify-center">
+            <Image
+              className="featured-image-wrapper w-full h-full object-cover rounded-none"
+              src={featuredImage.node.mediaItemUrl}
+              layout="fill"
+              // sizes="(min-width: 1024px) 50vw, 70vw"
+              loading="eager"
+              priority={true}
+              alt={featuredImage.node.altText || title}
+            />
+            <h1 className="absolute max-w-[480px] text-4xl font-medium !leading-none text-center md:max-w-[580px] md:text-5xl lg:max-w-[680px] lg:text-[58px]">
+              {title}
+            </h1>
           </div>
         </section>
+      )}
 
-        {!isPreview && <NextProject currentProject={project} />}
+      <section className="work-project flex flex-col bg-[#300808]">
+        <div className="w-full max-w-main mx-auto px-5 sm:px-12 pb-16 sm:pb-40">
+          {!featuredImage && (
+            <h1 className="mx-auto mt-12 mb-12 max-w-[480px] text-3xl font-medium !leading-none text-center md:max-w-[580px] md:text-4xl lg:max-w-[680px] lg:text-[38px] text-center">
+              {title}
+            </h1>
+          )}
+          {(project.content ||
+            projectsSingle?.projectDetails?.attributes?.length) && (
+            <div className="flex flex-col mt-8">
+              <div
+                className="flex items-center cursor-pointer"
+                onClick={() => setRevealProjectInfo((old) => !old)}
+              >
+                <div
+                  className={`relative flex items-center justify-center w-6 h-6 mr-2 sm:mr-6 transition-all ease-out duration-300 ${
+                    revealProjectInfo ? 'rotate-[135deg]' : 'rotate-0'
+                  }`}
+                >
+                  <div className="absolute w-4 h-0.5 bg-taupe"></div>
+                  <div className="absolute w-0.5 h-4 bg-taupe"></div>
+                </div>
+                <p className="text-taupe text-xl leading-[44px] sm:text-[26px]">
+                  {projectsSingle.projectDetails.label ||
+                    'Project Information'}
+                </p>
+              </div>
+              <div
+                className={`text-taupe w-full md:w-1/2 h-0 ${
+                  revealProjectInfo ? 'h-full pt-2' : 'pt-0'
+                } flex flex-col pl-8 sm:pl-12 pr-4 md:pr-0 overflow-hidden transition-all`}
+              >
+                {/* {project.content && (
+                  <div
+                    className="space-y-12"
+                    dangerouslySetInnerHTML={{
+                      __html: project.projectsSingle.projectDetails.content,
+                    }}
+                  />
+                )} */}
+                {projectsSingle.projectDetails.attributes && (
+                  <div className="project-details">
+                    {projectsSingle.projectDetails.attributes.map(
+                      (attribute, i) => (
+                        <div
+                          key={`project-details-${i}`}
+                          className="flex flex-col pt-9 space-y-2"
+                        >
+                          {attribute.label && (
+                            <p className="text-taupe text-xs">
+                              {attribute.label}
+                            </p>
+                          )}
+                          {attribute.attributeListings && (
+                            <div className="text-taupe text-lg leading-[34px] sm:text-[20px]">
+                              {attribute.attributeListings.map(
+                                (attItem, a) => {
+                                  return (
+                                    <div key={`project-attribute-${a}`}>
+                                      {attItem.link ? (
+                                        <a
+                                          target="_blank"
+                                          href={attItem.link}
+                                          rel="noreferrer"
+                                        >
+                                          <span>{attItem.title}</span>
+                                        </a>
+                                      ) : (
+                                        <>{attItem.title}</>
+                                      )}
+                                      {a !==
+                                        attribute.attributeListings.length -
+                                          1 && <span>, </span>}
+                                    </div>
+                                  );
+                                }
+                              )}
+                            </div>
+                          )}
+                        </div>
+                      )
+                    )}
+                  </div>
+                )}
+              </div>
+            </div>
+          )}
+          {projectImages.length ? (
+            <div
+              id="project-images-container"
+              ref={scrollContainerRef}
+              className="flex flex-col mt-8 md:mt-20"
+            >
+              {projectImages.map((block, i) => {
+                const blockPos = imageBlockPositions[i];
+                const blockSize = imageBlockSizes[i];
+                const imageBlock = block.image ? block.image : block.video;
+                if (!imageBlock) return <></>;
+                const orientation =
+                  imageBlock.node.mediaDetails.width <=
+                  imageBlock.node.mediaDetails.height
+                    ? 'portrait'
+                    : 'landscape';
+                    
+                return (
+                  <div
+                    ref={projectRefs[i]}
+                    className={`project-block${
+                      projectImages.length !== i + 1 ? ' mb-20 md:mb-40' : ''
+                    } flex flex-col-reverse items-start ${
+                      pos2Class[blockPos]
+                    } sm:items-center`}
+                    key={`project-image-${i}`}
+                  >
+                    <div className="description-reveal flex-1 text-taupe text-sm_extra leading-[24px]">
+                      {block.description && block.description}
+                    </div>
+                    {(block.image || block.video) && (
+                      <div
+                        className={`image-reveal image-to-lightbox ${size2Class[blockSize]} relative ${orientation === 'landscape'
+                          ? 'aspect-[4/3]'
+                          : 'aspect-[3/4]'}`}
+                        onClick={() =>
+                          !isMobile ? setClickedImageOrder(i) : null
+                        }
+                      >
+                        {block.video ? (
+                          <div>
+                            <video
+                              autoPlay
+                              muted
+                              loop
+                              onLoadedData={() =>
+                                setImageBlockDetails(
+                                  imageBlockDetails.map((item, index) =>
+                                    index === i
+                                      ? { ...item, isLoaded: true }
+                                      : item
+                                  )
+                                )
+                              }
+                              className="w-full h-full"
+                            >
+                              <source
+                                src={block.video.node.mediaItemUrl}
+                                type="video/mp4"
+                              />
+                            </video>
+                          </div>
+                        ) : (
+                          <Image
+                            className={`featured-image-wrapper w-full h-auto rounded featured-image-${block.image.node.id}`}
+                            src={block.image.node.mediaItemUrl}
+                            layout="fill"
+                            objectFit="cover"
+                            objectPosition="center"
+                            sizes="(min-width: 1024px) 40vw, 70vw"
+                            loading="eager"
+                            onLoad={event => handleDisplayImage(event, projectRefs[i])}	
+                            quality={82}
+                            alt={
+                              block.image.node.altText ||
+                              block.description ||
+                              `${title} image ${i + 1}`
+                            }
+                          />
+                        )}
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          ) : <></>}
+        </div>
+      </section>
 
-        {clickedImageOrder > -1 && (
-          <ProjectCarouselModal
-            imageBlocks={projectImages}
-            initialSlide={clickedImageOrder}
-            onClose={() => setClickedImageOrder(-1)}
-          />
-        )}
-      </>
-    // </PageLayout>
+      {!isPreview && <NextProject currentProject={project} />}
+
+      {clickedImageOrder > -1 && (
+        <ProjectCarouselModal
+          imageBlocks={projectImages}
+          initialSlide={clickedImageOrder}
+          onClose={() => setClickedImageOrder(-1)}
+        />
+      )}
+    </>
   );
 };
 
