a0948fd8bda1edc014570a1fc7bb57b09b5adf38
Brandon Phillips <brandon@fullmetalworkshop.com>
2024-07-03 15:31:27 -0400

    404 page

---
+++ pages/404.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import Link from 'next/link'
+import { PageLayout } from '../components';
+
+const NotFoundPage = () => {
+  return (
+    <PageLayout className="relative">
+      <main className="bg-dark_blue h-screen">
+        <section className="relative w-full max-w-main mx-auto px-5 sm:px-12 py-32 sm:py-32">
+          <h1 className="text-xl">Oops, this page doesn’t exist.</h1>
+          <p>
+            View some of our{' '}
+            <Link href="/work">
+              case studies
+            </Link>{' '}
+            instead :)
+          </p>
+        </section>
+      </main>
+    </PageLayout>
+  );
+};
+
+export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>;
